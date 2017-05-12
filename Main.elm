eded9f5016289e42d78aa1e99e80861e6b1bddfa
vishalgautamm <vishal.g@live.ca>
2017-05-12 11:21:01 -0400

    Stylized Components using Material Framework

--- Main.elm
+++ Main.elm
@@ -26,6 +26,7 @@ import Material.Options as Options exposing (css)
 type alias Model =
     { subreddit : Subreddit
     , posts : List Post
+    , mdl : Material.Model
     }
 
 
@@ -38,12 +39,13 @@ type alias Post =
     , url : String
     , permalink : String
     , id : String
+    , comments : Int
     }
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( Model (Subreddit "Elm") [], Cmd.none )
+    ( Model (Subreddit "Elm") [] Material.model, Cmd.none )
 
 
 
@@ -54,6 +56,7 @@ type Msg
     = OpenReddit (Result Http.Error (List Post))
     | GetReddit
     | UpdateReddit String
+    | Mdl (Material.Msg Msg)
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -71,6 +74,9 @@ update msg model =
         UpdateReddit string ->
             ( { model | subreddit = (updateSelection string) }, Cmd.none )
 
+        Mdl action_ ->
+            Material.update Mdl action_ model
+
 
 updateSelection : String -> Subreddit
 updateSelection string =
@@ -83,24 +89,99 @@ updateSelection string =
 
 view : Model -> Html Msg
 view model =
+    Scheme.topWithScheme Color.Cyan Color.Lime <|
+        Layout.render Mdl
+            model.mdl
+            [ Layout.fixedHeader ]
+            { header = [ header ]
+            , drawer = []
+            , tabs = ( [], [] )
+            , main = [ viewContent model ]
+            }
+
+
+header : Html Msg
+header =
     div []
-        [ div []
-            [ input [ type_ "text", placeholder "subreddit", onInput UpdateReddit ] []
-            , button [ onClick GetReddit ] [ text "Go" ]
-            , h3 [] [ text model.subreddit.name ]
-            , h3 [] [ text <| "https://www.reddit.com/r/" ++ model.subreddit.name ]
-            , div [] <| List.map postView model.posts
+        [ h5
+            [ style
+                [ ( "float", "left" )
+                , ( "padding-left", "40px" )
+                ]
+            ]
+            [ text "Reddit SPA" ]
+        ]
+
+
+containerStyle : List (Options.Property a b)
+containerStyle =
+    [ css "margin" "auto"
+    , css "padding-left" "10%"
+    , css "padding-right" "10%"
+    ]
+
+
+viewContent : Model -> Html Msg
+viewContent model =
+    Options.div containerStyle
+        [ div
+            []
+            [ Textfield.render Mdl
+                [ 0 ]
+                model.mdl
+                [ Options.onInput UpdateReddit
+                , Textfield.value model.subreddit.name
+                , css "width" "800px"
+                , css "margin-left" "15px"
+                ]
+                []
+            , Button.render Mdl
+                [ 1 ]
+                model.mdl
+                [ Options.onClick GetReddit
+                , Button.raised
+                , Button.ripple
+                , Button.colored
+                , Button.accent
+                , css "margin-left" "10px"
+                , css "width" "110px"
+                ]
+                [ text "Go!" ]
+            , h5 [] [ text model.subreddit.name ]
+            , h5 [] [ text <| "https://www.reddit.com/r/" ++ model.subreddit.name ]
+            , div [ class "wrap-posts" ]
+                [ section []
+                    [ Lists.ul [] (List.map postView model.posts) ]
+                ]
             ]
         ]
 
 
 postView : Post -> Html Msg
 postView post =
-    div []
-        [ a [ href post.url ] [ text post.title ]
-        , div [] [ text "<" ]
-        , a [ href <| "https://www.reddit.com" ++ post.permalink ] [ text "Comments!" ]
-        , div [] [ text "<<=======================================================>>" ]
+    Lists.li []
+        [ Lists.content []
+            [ a
+                [ style
+                    [ ( "color", "rgba(0, 0, 0, 0.72)" )
+                    , ( "font-weight", "500" )
+                    ]
+                , href post.url
+                , target "_blank"
+                ]
+                [ text post.title ]
+            ]
+        , Lists.content2 []
+            [ a
+                [ style
+                    [ ( "color", "rgba(0,0,0,0.72)" )
+                    , ( "font-weight", "500" )
+                    ]
+                , href <| "https://www.reddit.com" ++ post.permalink
+                , target "_blank"
+                ]
+                [ text <| "Comments! (" ++ toString post.comments ++ ")" ]
+            ]
         ]
 
 
@@ -140,11 +221,12 @@ decodeReddit =
 
 decodePost : Json.Decoder Post
 decodePost =
-    Json.map4 Post
+    Json.map5 Post
         (Json.at [ "data", "title" ] Json.string)
         (Json.at [ "data", "url" ] Json.string)
         (Json.at [ "data", "permalink" ] Json.string)
         (Json.at [ "data", "id" ] Json.string)
+        (Json.at [ "data", "num_comments" ] Json.int)
 
 
 main =
